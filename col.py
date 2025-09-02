398150ad1f819f951229650dc42c02a365c1af6e
PeterGrecian <peter.grecian@gmail.com>
2025-09-02 12:54:13 +0100

    predictions understood

--- col.py
+++ col.py
@@ -1,9 +1,7 @@
 # open a file and read lines of csv data
 import csv
 
-# return false if not int
-def isint(i):
-    
+places_of_drivers = {}
 
 # Open the CSV file in read mode
 with open('f1-20250907.csv', mode='r') as file:
@@ -11,5 +9,21 @@ with open('f1-20250907.csv', mode='r') as file:
 
     # Loop through the lines in the CSV file
     for row in csv_reader:
-        print(row)  # Each row will be a list of values from the CSV file
+        try:
+            i = int(row[0])
+        except (TypeError, ValueError, IndexError):
+            continue # next
 
+        print(row)  # Each row will be a list of values from the CSV file
+        driver = row[1].split()[-1]
+        place = row[0]
+
+
+        if driver not in places_of_drivers:
+            places_of_drivers[driver] = []
+
+        places_of_drivers[driver].append(place)
+
+from pprint import pprint
+pprint(places_of_drivers)
+print(len(places_of_drivers))
